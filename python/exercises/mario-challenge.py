b63df85e7ba5b205b91f1b5c7d3b387d3aca6f05
kevensouzz <souzadossantoskeven@gmail.com>
2024-01-30 19:11:38 -0300

    upload mario-challenge.py

---
+++ python/exercises/mario-challenge.py
@@ -0,0 +1,17 @@
+def pyramid(height):
+    for i in range(0, height, +1):
+        for j in range(height, i + 1, -1):
+            print(" ", end="")
+        for k in range(i + 1):
+            print("#", end="")
+        print("  ", end="")
+        for l in range(i + 1):
+            print("#", end="")
+        print()
+
+
+height = 0
+while height > 8 or height <= 0:
+    height = int(input("height: "))
+
+pyramid(height)
