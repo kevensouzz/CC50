40bd55f11e3a4bb9cd14aa4e6c358a85b6e06916
kevensouzz <souzadossantoskeven@gmail.com>
2024-02-01 19:07:35 -0300

    dna exercise

---
+++ python/exercises/dna/dna.py
@@ -0,0 +1,45 @@
+from sys import argv
+import csv
+
+
+def dna_identification(strs, dna):
+    header = strs[0]
+    str_info = header[1:]
+
+    str_count = [dna.count(word) for word in str_info]
+
+    str_values = strs
+    del str_values[0]
+
+    str_names = []
+    for row in str_values:
+        str_names.append(row[0])
+
+    for row in str_values:
+        del row[0]
+
+    for index, sublist in enumerate(str_values):
+        if list(map(int, sublist)) == str_count:
+            response = str_names[index]
+            break
+        else:
+            response = "No Match!"
+
+    print(response)
+
+
+if len(argv) != 3:
+    print("\nUSAGE: dna.py data.csv sequence.txt\n")
+else:
+    database_content = []
+
+    with open(argv[1], "r") as database_file:
+        database_reader = csv.reader(database_file)
+
+        for row in database_reader:
+            database_content.append(row)
+
+    with open(argv[2], "r") as sequence_file:
+        sequence_content = sequence_file.read()
+
+    dna_identification(database_content, sequence_content)
